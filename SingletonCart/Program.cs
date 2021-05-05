cd73396bcfad6701db9b2b85644a6df38ab8707a
Podojohn <johnpodogorianiotis@gmail.com>
2021-05-05 07:35:36 +0300

    Singleton Cart

---
+++ SingletonCart/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonCart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    sealed class CartSingleton
+    {
+
+        private static int counter = 0;
+        private CartSingleton()
+        {
+            counter++;
+            Console.WriteLine("Created a new one");
+        }
+
+        private static readonly CartSingleton _CartInstance = new CartSingleton();
+
+        public static CartSingleton CartInstance
+        {
+            get
+            {
+                return _CartInstance;
+            }
+
+        }
+
+
+    }
+}
